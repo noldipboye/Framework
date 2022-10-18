e3568c900ce0e63c5ea7ccc47762d776f1dacdd4
Nolan D <38262360+noldipboye@users.noreply.github.com>
2022-10-18 10:03:20 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+function OnN() {
+    document.getElementById("dropdown_items").style.display="block";
+    document.getElementById("dropdown_button").style.display="none";
+    document.getElementById("dropdown_button2").style.display="block";
+    document.getElementById("dropdown_button2").style.display="flex";
+}
+
+function OffN() {
+    document.getElementById("dropdown_items").style.display="none";
+    document.getElementById("dropdown_button").style.display="block";
+    document.getElementById("dropdown_button2").style.display="none";
+    document.getElementById("dropdown_button").style.display="flex";
+    
+}
