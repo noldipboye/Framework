1503188246587680f4cfe64ede936e2ba8f1ee86
Nolan D <38262360+noldipboye@users.noreply.github.com>
2022-10-20 09:17:08 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -12,3 +12,16 @@ function OffN() {
     document.getElementById("dropdown_button").style.display="flex";
     
 }
+
+function Off() {
+    document.getElementById("Accept").style.display="block";
+    document.getElementById("Modal-Body").style.display="none";
+    document.getElementById("Decline").style.display="none";
+}
+
+function On() {
+    document.getElementById("Decline").style.display="block";
+    document.getElementById("Modal-Body").style.display="block";
+    document.getElementById("Accept").style.display="none";
+
+}
